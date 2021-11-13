93f6ecc96a6f9cbf935ee90d7f064cbc4dc30844
R. Fadhil <ridhanfadhilah@gmail.com>
2021-11-13 20:36:53 +0700

    score system

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import store from '../store';
 import { Button } from "@elevenia/master-ui/components/Atom";
+import styled from 'styled-components';
+
+const StyledGame = styled.div`
+    position: absolute;
+    left: 5px;
+    right: 5px;
+    bottom: 5px;
+`
 
 const Game = () => {
-    const [ score, setScore ] = useState(0);
+    const [ score, setScore ] = useState(10);
 
     const scoreChangeHandler = (action, value) => {
         switch(action) {
@@ -19,20 +27,29 @@ const Game = () => {
     const storeHandler = async () => {
         try {
             const STORE_SCORE = await store.actions.game.storeScore();
-            console.log(STORE_SCORE.statusText);
-            console.log(score);
+            console.log(STORE_SCORE.status, STORE_SCORE.statusText);
+            console.log("Score: ", score);
         } catch (error) {
             
         }
     }
 
+    useEffect(() => {
+        if (score < 10) {
+            setScore(10);
+        } else if (score > 500) {
+            setScore(500);
+        }
+    }, [score])
+
     return (
-        <div>
-            <Button variant="secondary" onClick={() => scoreChangeHandler("increase", 100)}>Increase Score</Button>
-            <Button variant="secondary" onClick={() => scoreChangeHandler("decrease", 50)}>Decrease Score</Button>
+        <StyledGame>
             <p>Score: {score}</p>
+            <Button variant="secondary" onClick={() => scoreChangeHandler("increase", 5)}>Bola Ungu</Button>
+            <Button variant="secondary" onClick={() => scoreChangeHandler("increase", 1)}>Bola kuning</Button>
+            <Button variant="secondary" onClick={() => scoreChangeHandler("decrease", 2)}>Bola hitam</Button>
             <Button variant="secondary" onClick={storeHandler}>SIMPAN</Button>
-        </div>
+        </StyledGame>
     )
 }
 
